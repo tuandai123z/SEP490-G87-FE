dd25931cd39512f42cd9356c61fdbc6a2426c712
tuandai123z <tuanbmhe161164@fpt.edu.vn>
2025-03-11 23:48:56 +0700

    Update Pagination

--- Dashboard/src/components/common/Pagination.jsx
+++ Dashboard/src/components/common/Pagination.jsx
@@ -1,11 +1,14 @@
 
 const Pagination = ({ totalPages, size, currentPage, onPageChange, content }) => {
+    const totalPagePagination = totalPages % size > 0 ? totalPages / size + 1 : totalPages / size;
 
     return (
         <nav className="flex flex-wrap items-center justify-between pt-4 flex-column md:flex-row" aria-label="Table navigation">
             <span className="block w-full mb-4 text-sm font-normal text-gray-500 dark:text-gray-400 md:mb-0 md:inline md:w-auto">Hiển thị <span className="font-semibold text-gray-900 dark:text-white">{1 + size * (currentPage - 1)}-{(size + size * (currentPage - 1) <= totalPages ? size + size * (currentPage - 1) : totalPages)}</span> trong <span className="font-semibold text-gray-900 dark:text-white">{totalPages} </span>{content}</span>
             <ul className="inline-flex h-8 -space-x-px text-sm rtl:space-x-reverse">
-                <li onClick={() => onPageChange(currentPage - 1)}>
+                <li onClick={() => {
+                    if (currentPage > 1) onPageChange(currentPage - 1)
+                }}>
                     <a href="#" className="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 ms-0 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Trước</a>
                 </li>
                 {Array.from({ length: (totalPages % size > 0 ? totalPages / size + 1 : totalPages / size) }).map((_, index) => (
@@ -16,7 +19,11 @@ const Pagination = ({ totalPages, size, currentPage, onPageChange, content }) =>
                             }`}>{index + 1}</a>
                     </li>
                 ))}
-                <li onClick={() => onPageChange(currentPage + 1)}>
+                <li onClick={() => {
+                    if (totalPagePagination > currentPage + 1) {
+                        onPageChange(currentPage + 1)
+                    }
+                }}>
                     <a href="#" className="flex items-center justify-center h-8 px-3 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Sau</a>
                 </li>
 
