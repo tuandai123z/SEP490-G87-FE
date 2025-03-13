f20752332f2ca8180fb45b2bc0f0fc4282c38edc
Do Hong Minh <doohonggminhh1310@gmail.com>
2025-03-13 23:02:16 +0700

    delete source landing page

--- LandingPage/src/utils/axiosInstant.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/',
-  timeout: 10000,
-});
-
-export const defaultBaseUrl = 'http://localhost:8080/';
--- LandingPage/src/utils/format.js
+++
@@ -1,4 +0,0 @@
-export const formatVND = (number) => {
-  return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(number);
-};
-
