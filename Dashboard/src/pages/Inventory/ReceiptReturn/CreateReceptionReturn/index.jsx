ab7bfd8bc91cd82ec60215b7a0f5ba0e4c14b165
minhdh <doohonggminhh1310@gmail.com>
2025-04-12 14:47:56 +0700

    remove location in the reciept

--- Dashboard/src/pages/Inventory/ReceiptReturn/CreateReceptionReturn/index.jsx
+++ Dashboard/src/pages/Inventory/ReceiptReturn/CreateReceptionReturn/index.jsx
@@ -81,19 +81,19 @@ const CreateReceptionReturn = () => {
         setListProducts(updatedProducts);
     }
 
-    const handleChangeLocation = (index, value) => {
-        const updatedProducts = [...listProducts];
-        updatedProducts[index].location = value;
-        setListProducts(updatedProducts);
-    }
+    // const handleChangeLocation = (index, value) => {
+    //     const updatedProducts = [...listProducts];
+    //     updatedProducts[index].location = value;
+    //     setListProducts(updatedProducts);
+    // }
 
     const handleCreateReceptionReturn = () => {
         const listProductPurchase = listProducts?.map(product => {
             const productItem = {
                 productCode: product.productCode,
-                dateOfManufacture: product.dateOfManufacture,
-                dateOfExpiry: product.dateOfExpiry,
-                location: product.location,
+                dateOfManufacture: product.dateOfManufacture|| "",
+                dateOfExpiry: product.dateOfExpiry|| "",
+                location: product.location || "",
                 quantityReturn: product.quantityReturn,
                 status: product.statusProduct
             }
@@ -130,26 +130,27 @@ const CreateReceptionReturn = () => {
             toast.warn("Vui lòng chọn phiếu hoàn hàng");
             return;
         }
-        let isAvailableDate = listProducts?.length !== 0 && listProducts?.every(product => (
-            product.dateOfManufacture < product.dateOfExpiry
-        ))
+        // let isAvailableDate = listProducts?.length !== 0 && listProducts?.every(product => (
+        //     product.dateOfManufacture < product.dateOfExpiry
+        // ))
 
 
-        if (!isAvailableDate) {
-            toast.warn("HSD và NSX không hợp lệ!")
-            return;
-        }
+        // if (!isAvailableDate) {
+        //     toast.warn("HSD và NSX không hợp lệ!")
+        //     return;
+        // }
 
-        let allowOpen = listProducts?.length !== 0 && listProducts?.some(product => (
-            product.location === ''
-        ))
-        if (allowOpen) {
-            toast.warn("Vui lòng điền đầy đủ thông tin thiết bị")
-            return;
-        }
+        // let allowOpen = listProducts?.length !== 0 && listProducts?.some(product => (
+        //     product.location === ''
+        // ))
+        // if (allowOpen) {
+        //     toast.warn("Vui lòng điền đầy đủ thông tin thiết bị")
+        //     return;
+        // }
 
 
-        !allowOpen && isAvailableDate && setIsOpenModal(true);
+        // !allowOpen && isAvailableDate && 
+        setIsOpenModal(true);
     }
 
     useEffect(() => {
@@ -210,9 +211,8 @@ const CreateReceptionReturn = () => {
                                 <th scope="col" className="px-6 py-3 border border-blue-300">Mã hàng</th>
                                 <th scope="col" className="px-6 py-3 border border-blue-300">Tên hàng</th>
                                 <th scope="col" className="px-6 py-3 border border-blue-300">ĐVT</th>
-                                <th scope="col" className="px-6 py-3 border border-blue-300">NSX<span className="pl-1 text-lg font-semibold text-red">*</span></th>
-                                <th scope="col" className="px-6 py-3 border border-blue-300">HSD<span className="pl-1 text-lg font-semibold text-red">*</span></th>
-                                <th scope="col" className="px-6 py-3 border border-blue-300">Vị trí<span className="pl-1 text-lg font-semibold text-red">*</span></th>
+                                <th scope="col" className="px-6 py-3 border border-blue-300">NSX</th>
+                                <th scope="col" className="px-6 py-3 border border-blue-300">HSD</th>
                                 <th scope="col" className="px-6 py-3 border border-blue-300">Số lượng</th>
                                 <th scope="col" className="px-6 py-3 border border-blue-300 ">Giá nhập<span className="pl-1 text-lg font-semibold text-red">*</span></th>
                                 <th scope="col" className="px-6 py-3 border border-blue-300">Thành tiền</th>
@@ -243,15 +243,6 @@ const CreateReceptionReturn = () => {
                                                 className="w-full px-2 py-1 border border-gray-300 rounded"
                                             />
                                         </td>
-                                        <td className="px-6 py-4 border border-blue-300">
-                                            <input
-                                                type="text"
-                                                placeholder="Nhập vị trí..."
-                                                value={item?.location}
-                                                onChange={(e) => handleChangeLocation(index, e.target.value)}
-                                                className="w-full px-2 py-1 border border-gray-300 rounded"
-                                            />
-                                        </td>
                                         <td className="px-6 py-4 border border-blue-300">{item?.quantityReturn}</td>
                                         <td className="px-4 py-4 border border-blue-300">{formatVND((1 - (item?.discount / 100)) * item?.productInformation?.sellingPrice)}</td>
                                         <td className="px-6 py-4 border border-blue-300">{formatVND(item?.quantityReturn * (1 - (item?.discount / 100)) * item?.productInformation?.sellingPrice)}</td>
@@ -267,14 +258,12 @@ const CreateReceptionReturn = () => {
                                     <td className="px-6 py-4 border border-l-0 border-r-0 border-blue-300"></td>
                                     <td className="px-6 py-4 border border-l-0 border-r-0 border-blue-300"></td>
                                     <td className="px-6 py-4 border border-l-0 border-r-0 border-blue-300"></td>
-                                    <td className="px-6 py-4 border border-l-0 border-r-0 border-blue-300"></td>
                                     <td className="px-6 py-4 border border-r-0 border-blue-300">{formatVND(totalCost)}</td>
                                 </tr>
                             )}
                             {listProducts?.length === 0 && (
                                 <tr className="text-black border border-b border-blue-400" >
                                     <th scope="row" className="px-6 py-4 font-medium text-black border border-blue-300 whitespace-nowrap">Chưa có phiếu nào được chọn</th>
-                                    <td className="px-6 py-4 border border-blue-300"></td>
                                     <td className="px-6 py-4 border border-blue-300"></td>
                                     <td className="px-6 py-4 border border-blue-300"></td>
                                     <td className="px-6 py-4 border border-blue-300"></td>
