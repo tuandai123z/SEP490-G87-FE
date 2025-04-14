d3832e6e5c86a2398416bda8a6b51885a1889b81
tuandai123z <tuanbmhe161164@fpt.edu.vn>
2025-04-14 23:47:54 +0700

    add chart

--- Dashboard/src/pages/Dashboard/index.jsx
+++ Dashboard/src/pages/Dashboard/index.jsx
@@ -3,15 +3,20 @@ import { LuPackage } from "react-icons/lu";
 import { FiUserPlus } from "react-icons/fi";
 import { PiMoneyWavy } from "react-icons/pi";
 import MyChart from "../MyChart";
+import { axiosInstance } from "../../utils/axiosInstant";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+import { useState } from "react";
+import PieChart from "../MyChart/PieChart";
 
 const TopDashboard = () => {
     return (
         <div className="flex flex-col w-full">
             <div className='flex items-center justify-between'>
-                <h1 className='text-[28px] leading-[34px] font-normal text-[#5a5c69] cursor-pointer'>Thống kê</h1>
+                <h1 className='text-[28px] leading-[34px] font-normal text-[#5a5c69] cursor-pointer'>Thống kê kho hàng</h1>
 
             </div>
-            <div className='grid grid-cols-3 gap-[30px] mt-[25px] pb-[15px] '>
+            {/* <div className='grid grid-cols-3 gap-[30px] mt-[25px] pb-[15px] '>
                 <div className=' h-[100px] rounded-[8px] bg-white border-l-[4px] border-[#4E73DF] flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out'>
                     <div>
                         <h2 className='text-[#B589DF] text-[11px] leading-[17px] font-bold uppercase'>Doanh thu</h2>
@@ -36,7 +41,7 @@ const TopDashboard = () => {
                     <LuPackage fontSize={28} />
                 </div>
 
-            </div>
+            </div> */}
         </div>)
 }
 
@@ -44,10 +49,94 @@ const TopDashboard = () => {
 
 const Dashboard = () => {
 
+    const [dataReturn, setDataReturn] = useState([])
+    const [dataReceipt, setDataReceipt] = useState([])
+    const [dataDelivery, setDataDelivery] = useState([])
+    const totalFormReceipt = dataReceipt?.reduce((sum, item) => sum + Number(item?.totalFormInMonth), Number(0));
+    const totalFormDelivery = dataDelivery?.reduce((sum, item) => sum + Number(item?.totalFormInMonth), Number(0));
+    const dataPieChart = [
+        {
+            name: `Xuất kho`,
+            y: totalFormDelivery
+        },
+        {
+            name: `Nhập kho`,
+            y: totalFormReceipt
+        }
+    ]
+
+    const getDataStatisticReturn = () => {
+        axiosInstance
+            .get(`/statistic/return`)
+            .then(res => {
+                const data = res.data;
+                setDataReturn(data);
+            }).catch((err) => {
+                if (err.response) {
+                    const errorRes = err.response.data;
+                    toast.error(errorRes.message);
+                } else if (err.request) {
+                    toast.error(err.request);
+                } else {
+                    toast.error(err.message);
+                }
+            });
+    }
+
+    const getDataStatisticReceipt = () => {
+        axiosInstance
+            .get(`/statistic/receipt`)
+            .then(res => {
+                const data = res.data;
+                setDataReceipt(data);
+                console.log(data);
+            }).catch((err) => {
+                if (err.response) {
+                    const errorRes = err.response.data;
+                    toast.error(errorRes.message);
+                } else if (err.request) {
+                    toast.error(err.request);
+                } else {
+                    toast.error(err.message);
+                }
+            });
+    }
+
+    const getDataStatisticDelivery = () => {
+        axiosInstance
+            .get(`/statistic/delivery`)
+            .then(res => {
+                const data = res.data;
+                setDataDelivery(data);
+            }).catch((err) => {
+                if (err.response) {
+                    const errorRes = err.response.data;
+                    toast.error(errorRes.message);
+                } else if (err.request) {
+                    toast.error(err.request);
+                } else {
+                    toast.error(err.message);
+                }
+            });
+    }
+
+    useEffect(() => {
+        getDataStatisticReturn();
+        getDataStatisticReceipt();
+        getDataStatisticDelivery();
+    }, [])
+
+
+
     return (
-        <div className="w-[98%] h-auto">
+        <div className="w-[98%] h-auto flex flex-col gap-4">
             <TopDashboard />
-            <MyChart />
+            <div className="grid w-full grid-cols-2 gap-4">
+                <div className="col-span-1"><MyChart data={dataReceipt} title={"Biểu đồ thống kê nhập kho"} type='column' /></div>
+                <div className="col-span-1"><MyChart data={dataDelivery} title={"Biểu đồ thống kê xuất kho"} type='column' /></div>
+                <div className="col-span-1"><MyChart data={dataReturn} title={"Biểu đồ thống kê hoàn hàng"} type='column' /></div>
+                <div className="col-span-1"><PieChart data={dataPieChart} title={"Biểu đồ thống kê tỉ lệ xuất nhập kho"} /></div>
+            </div>
         </div>)
 };
 
--- Dashboard/src/pages/MyChart/index.jsx
+++ Dashboard/src/pages/MyChart/index.jsx
@@ -2,22 +2,73 @@ import React from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
+const MyChart = ({ data, title, type }) => {
+    const months = data && data?.map(item => {
+        return `Tháng ${item?.month}`
+    });
+    const totalAmounts = data.map(item => item.totalAmount);
+    const totalForms = data.map(item => item.totalFormInMonth);
 
-const options = {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: "Biểu đồ thống kê",
-    },
-    series: [
-        {
-            name: "Dữ liệu",
-            data: [1, 3, 2, 4, 3, 1, 2, 5, 3, 1, 2, 5],
+    const options = {
+        chart: {
+            type: type
         },
-    ],
-};
+        title: {
+            text: title
+        },
+        xAxis: {
+            categories: months,
+            title: {
+                text: "Tháng"
+            }
+        },
+        yAxis: [
+            {
+                title: {
+                    text: "Tổng tiền"
+                },
+                labels: {
+                    format: "{value}đ"
+                }
+            },
+            {
+                title: {
+                    text: "Tổng số đơn"
+                },
+                opposite: true, // hiện ở bên phải
+                labels: {
+                    format: "{value} đơn"
+                }
+            }
+        ],
+        tooltip: {
+            shared: true
+        },
+        plotOptions: {
+            column: {
+                grouping: true,
+                shadow: false,
+                borderWidth: 0
+            }
+        },
+        series: [
+            {
+                name: "Tổng tiền",
+                data: totalAmounts,
+                color: "#434348"
+            },
+            {
+                name: "Tổng số đơn",
+                data: totalForms,
+                yAxis: 1,
+                color: "#7cb5ec"
+            }
+        ]
+    };
 
-const MyChart = () => <HighchartsReact highcharts={Highcharts} options={options} />;
+    return (
+        <HighchartsReact highcharts={Highcharts} options={options} />
+    )
+};
 
 export default MyChart;
